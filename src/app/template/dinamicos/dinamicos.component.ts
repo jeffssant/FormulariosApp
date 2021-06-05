f94659ef2262fd7d3f3395f6e192f773ec42f6a1
jeffssant <santana.jeff@gmail.com>
2021-06-05 13:23:59 -0300

    Adicionar e eliminar itens

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,13 +18,18 @@ import { NgForm } from '@angular/forms';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  {
 
   @ViewChild('miForm') miForm!: NgForm;
 
-  constructor() { }
+  nuevoJuego: string = '';
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: "Jeff",
+    favoritos: [
+      {id:1, nombre: "Street fighter"},
+      {id:2, nombre: "Mario Kart"}
+    ]
   }
 
   guardar(){
@@ -23,5 +39,23 @@ export class DinamicosComponent implements OnInit {
   nombreValido():boolean {
     return this.miForm?.controls.name?.invalid && this.miForm?.controls.name?.touched
   } 
+
+  eliminar(i: number) {
+    this.persona.favoritos.splice(i,1)
+  }
+
+  agregarJuego() {
+    if(!this.nuevoJuego){
+      return;
+    }
+    
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length+1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+  }
 
 }
