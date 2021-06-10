f65cf905c7b106a616fc1a680d641a73f645f66f
jeffssant <santana.jeff@gmail.com>
2021-06-10 19:35:28 -0300

    Switch reativo

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miForm: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue]
+  })
+
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miForm.reset(this.persona);    
+
+    // Subiscreve as modificaçoes do formularios
+
+    this.miForm.valueChanges.subscribe(
+      ({condiciones, ...restoDeArgumentos})=> {
+        //delete form.condiciones;
+        this.persona = restoDeArgumentos
+       
+      }
+    )
   }
+
+  guardar() {
+    const formValue = {...this.miForm.value}
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+  }
+  
 
 }
