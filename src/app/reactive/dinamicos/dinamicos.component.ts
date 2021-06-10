88bc5899727ebe46041dddc37273302323d5a5d3
jeffssant <santana.jeff@gmail.com>
2021-06-10 17:50:01 -0300

    adicionar novo jogo

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, Validator, FormArray, Form, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -11,7 +11,17 @@ export class DinamicosComponent {
 
   miForm: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.minLength(3)]],   
+    favoritos: this.fb.array([
+      ['Street Fighter', Validators.required],
+      ['Mario Kart', Validators.required],
+    ], Validators.required)
   })
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  get favoritosArr() {
+    return this.miForm.get('favoritos') as FormArray;
+  }
 
 
   constructor(private fb: FormBuilder) { }
@@ -19,6 +29,15 @@ export class DinamicosComponent {
 
   campoEsValido(campo: string) {
     return this.miForm.controls[campo].errors && this.miForm.controls[campo].touched
+  }
+
+  agregarFavorito() {
+    if(this.nuevoFavorito.invalid){
+      return;
+    }
+
+    this.favoritosArr.push(this.fb.control(this.nuevoFavorito.value, Validators.required));
+    this.nuevoFavorito.reset()
   }
 
   guardar() {
