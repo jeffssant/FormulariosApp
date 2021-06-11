ff53de27635d32b97071d8b7db67b2820175224c
jeffssant <santana.jeff@gmail.com>
2021-06-11 12:43:34 -0300

    validaçao simples de usuario

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, Validator } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -12,9 +12,19 @@ export class RegistroComponent implements OnInit {
   nombreApelidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
+  noPuedeSerStrider (control: FormControl) {
+    const valor: string = control.value?.trim().toLowerCase();
+    if(valor === 'strider'){
+      return { noStrider:true }
+    }
+
+    return null;
+  }
+
   miForm: FormGroup = this.fb.group({
     nombre:['', [Validators.required, Validators.pattern(this.nombreApelidoPattern)]],
     email:['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    username:['', [Validators.required, this.noPuedeSerStrider]],
   })
 
   constructor(private fb: FormBuilder) { }
