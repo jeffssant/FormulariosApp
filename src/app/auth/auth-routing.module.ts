40d2521ecd5e35ea110020e175ab114270cc367c
jeffssant <santana.jeff@gmail.com>
2021-06-11 11:06:47 -0300

    Desenho da tela de login

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
         path: 'registro',
         component: RegistroComponent
       },
+      {
+        path: '**',
+        redirectTo: 'registro'
+      },
     ]
   
   }
