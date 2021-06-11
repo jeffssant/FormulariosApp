bc3e8ff7fb8a69515013383c7dd3174c42152368
jeffssant <santana.jeff@gmail.com>
2021-06-11 15:18:42 -0300

    validaçoes de senha

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -17,6 +17,13 @@ export class RegistroComponent implements OnInit {
     nombre:['', [Validators.required, Validators.pattern(this.validatorService.nombreApelidoPattern)]],
     email:['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
     username:['', [Validators.required, this.validatorService.noPuedeSerStrider]],
+    password:['',[Validators.required, Validators.minLength(6)]],
+    confirmarPass:['',Validators.required],
+  },  
+  {
+    validators: [
+      this.validatorService.camposIguales('password', 'confirmarPass')
+    ]
   })
 
   constructor(private fb: FormBuilder,
