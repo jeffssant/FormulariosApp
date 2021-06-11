ab83883a04d3beaa23187e4285972261683043ac
jeffssant <santana.jeff@gmail.com>
2021-06-11 11:49:18 -0300

    Validaçao de nome e email com expressao regular

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [RegistroComponent, LoginComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  nombreApelidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  miForm: FormGroup = this.fb.group({
+    nombre:['', [Validators.required, Validators.pattern(this.nombreApelidoPattern)]],
+    email:['', [Validators.required, Validators.pattern(this.emailPattern)]],
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miForm.reset({
+      nombre: "Jeff Sant",
+      email: "jeff@teste.com",
+    })
+  }
+
+  campoNoValido(campo: string){
+    return this.miForm.get(campo)?.invalid && this.miForm.get(campo)?.touched
+  }
+
+  submit() {
+    console.log(this.miForm.value);
+    this.miForm.markAllAsTouched();
   }
 
 }
