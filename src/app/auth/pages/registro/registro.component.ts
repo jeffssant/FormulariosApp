e62f73130e518f87dc993942bbf4e4ba1538e544
jeffssant <santana.jeff@gmail.com>
2021-06-14 20:15:51 -0300

    Mensagem de erro personalizada

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { ValidatorService } from '../../../shared/validator/validator.service';
 import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
+import { emailPattern } from '../../../shared/validator/validaciones';
 
 @Component({
   selector: 'app-registro',
@@ -27,6 +28,25 @@ export class RegistroComponent implements OnInit {
     ]
   })
 
+  get emailErrorsMsg():string {
+    const errors = this.miForm.get('email')?.errors;
+
+    if(errors?.required){
+      return 'Email es obligatorio'
+    }
+
+    else if(errors?.pattern){
+      return 'El valor ingressado no tiene formato de correo'
+    }
+
+    else if(errors?.emailTomado){
+      return 'El email ya fue tomado'
+    }
+
+
+    return "";
+  }
+
   constructor(private fb: FormBuilder,
               private validatorService: ValidatorService,
               private emailValidator: EmailValidatorService) { }
@@ -44,6 +64,8 @@ export class RegistroComponent implements OnInit {
   campoNoValido(campo: string){
     return this.miForm.get(campo)?.invalid && this.miForm.get(campo)?.touched
   }
+
+  
 
   submit() {
     console.log(this.miForm.value);
